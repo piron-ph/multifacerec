dd07afabe76dd6a9f0e3f5970f5d69f1f9e0a75b
Ligolas Neo Malicdem <Ligolasneo@icloud.com>
2024-04-04 00:56:08 +0800

    Update Login.cs

--- Login.cs
+++ Login.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using MySql.Data.MySqlClient;
 using System.Windows.Forms;
@@ -16,6 +17,9 @@ namespace MultiFaceRec
         Dashboard dash = new Dashboard();
         MySqlConnection connection = new MySqlConnection("datasource = localhost;port = 3306; Initial Catalog = 'e_log1'; username = root; password=");
         bool check;
+        int loginAttempts = 0;
+        DateTime cooldownEndTime;
+
         public Login()
         {
             InitializeComponent();
@@ -49,7 +53,17 @@ namespace MultiFaceRec
                 }
                 else
                 {
-                    MessageBox.Show("Invalid username or password!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    loginAttempts++;
+                    if (loginAttempts >= 5)
+                    {
+                        cooldownEndTime = DateTime.Now.AddMinutes(5);
+                        MessageBox.Show("Wrong password, please try again within 5 minutes", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        loginAttempts = 0; // will reset the login attempts after cooldown ends
+                    }
+                    else
+                    {
+                        MessageBox.Show("Invalid username or password!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
                 }
 
                 dr.Close();
@@ -63,7 +77,14 @@ namespace MultiFaceRec
 
         private void login_btn_Click(object sender, EventArgs e)
         {
-            LOGIN();
+            if (DateTime.Now < cooldownEndTime)
+            {
+                MessageBox.Show("Please wait for the cooldown period to finish.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                LOGIN();
+            }
         }
 
         private void cancel_btn_Click(object sender, EventArgs e)
@@ -78,9 +99,16 @@ namespace MultiFaceRec
 
         private void password_txbx_KeyDown(object sender, KeyEventArgs e)
         {
-            if(e.KeyCode == Keys.Enter)
+            if (e.KeyCode == Keys.Enter)
             {
-                LOGIN();
+                if (DateTime.Now < cooldownEndTime)
+                {
+                    MessageBox.Show("Please wait for the cooldown period to finish.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    LOGIN();
+                }
             }
         }
 
